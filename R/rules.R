993226dc41f8152de7dda551bc6f7fb231f63719
Gábor Csárdi <csardi.gabor@gmail.com>
2017-04-29 16:00:54 +0100

    rules

---
+++ R/rules.R
@@ -0,0 +1,175 @@
+
+#' @importFrom clisymbols symbol
+#' @importFrom crayon col_substring
+
+make_line <- function(x, char = symbol$line) {
+  cw <- col_nchar(char, "width")
+
+  ## We handle the simple case differently, to make it faster
+  if (cw == 1) {
+    paste(rep(char, x), collapse = "")
+
+  } else {
+    col_substring(
+      paste(rep(char, ceiling(x / cw)), collapse = ""),
+      1, x
+    )
+  }
+}
+
+#' Make a rule with one or two text labels
+#'
+#' The rule can include either a centered text label, or labels on the
+#' left and right side.
+#'
+#' To color the labels, use the functions from the `crayon` package
+#' directly, see examples below. To color the line, either use the
+#' `crayon` colors directly, or the `line_color` option.
+#'
+#' @param left Label to show on the left. It interferes with the `center`
+#'   label, only at most one of them can be present.
+#' @param center Label to show at the center. It interferes  with the
+#'   `left` and `right` labels.
+#' @param right Label to show on the right. It interferes with the `center`
+#'   label, only at most one of them can be present.
+#' @param line The character or string that is used to draw the line.
+#'   It can also `1` or `2`, to request a single line (Unicode, if
+#'   available), or a double line.
+#' @param line_color Either a color name (used in [crayon::make_style()]),
+#'   or a style function from `crayon`, to color the line.
+#' @param width Width of the rule. Defaults to the `width` option, see
+#'   [base::options()].
+#' @return Character scalar, the rule.
+#'
+#' @export
+#' @examples
+#'
+#' ## Simple rule
+#' cat(rule(), "\n")
+#'
+#' ## Double rule
+#' cat(rule(line = 2), "\n")
+#'
+#' ## Left label
+#' cat(rule(left = "Results"), "\n")
+#'
+#' ## Centered label
+#' cat(rule(center = " * RESULTS * "), "\n")
+#'
+#' ## Colored labels
+#' cat(rule(center = crayon::red(" * RESULTS * ")), "\n")
+#'
+#' ## Colored line
+#' cat(rule(center = crayon::red(" * RESULTS * "), line_color = "red"), "\n")
+#'
+#' ## Custom line
+#' cat(rule(center = "TITLE", line = "~"))
+#'
+#' ## More custom line
+#' cat(rule(center = "TITLE", line = crayon::blue("~-")), "\n")
+#'
+#' ## Even more custom line
+#' cat(rule(center = bgRed(" ", clisymbols::symbol$star, "TITLE",
+#'   clisymbols::symbol$star, " "),
+#'   line = "\u2582",
+#'   line_color = "orange"))
+
+rule <- function(left = "", center = "", right = "", line = 1,
+                 line_color = NULL, width = getOption("width")) {
+
+  options <- as.list(environment())
+
+  options$line <- get_line_char(options$line)
+  if (is.character(line_color)) line_color <- crayon::make_style(line_color)
+  if (is.function(line_color)) options$line <- line_color(options$line)
+
+  if (nchar(center)) {
+    if (nchar(left) || nchar(right)) {
+      stop(sQuote("center"), " cannnot be specified with ", sQuote("left"),
+           " or ", sQuote("right"))
+    }
+    rule_center(options)
+
+  } else if (nchar(left) && nchar(right)) {
+    rule_left_right(options)
+
+  } else if (nchar(left)) {
+    rule_left(options)
+
+  } else if (nchar(right)) {
+    rule_right(options)
+
+  } else {
+    rule_line(options)
+  }
+}
+
+get_line_char <- function(line) {
+  if (identical(line, 1) || identical(line, 1L)) {
+    symbol$line
+
+  } else if (identical(line, 2) || identical(line, 2L)) {
+    symbol$double_line
+
+  } else {
+    paste(as.character(line), collapse = "")
+  }
+}
+
+rule_line <- function(o) {
+  make_line(o$width, o$line)
+}
+
+#' @importFrom crayon col_nchar
+
+rule_center <- function(o) {
+
+  o$center <- col_substring(o$center, 1, o$width - 4)
+  o$center <- paste0(" ", o$center, " ")
+  ncc <- col_nchar(o$center, "width")
+
+  ndashes <- o$width - ncc
+
+  paste0(
+    make_line(ceiling(ndashes / 2), o$line),
+    o$center,
+    make_line(floor(ndashes / 2), o$line)
+  )
+}
+
+rule_left <- function(o) {
+  ncl <- col_nchar(o$left, "width")
+
+  paste0(
+    make_line(2, get_line_char(o$line)),
+    " ", o$left, " ",
+    make_line(o$width - ncl - 4, o$line)
+  )
+}
+
+rule_right <- function(o) {
+  ncr <- col_nchar(o$right, "width")
+
+  paste0(
+    make_line(o$width - ncr - 4, o$line),
+    " ", o$right, " ",
+    make_line(2, o$line)
+  )
+}
+
+rule_left_right <- function(o) {
+
+  ncl <- col_nchar(o$left, "width")
+  ncr <- col_nchar(o$right,  "width")
+
+  ## -- (ncl) -- (ncr) --
+  if (ncl + ncr + 10 > o$width) return(rule_left(o))
+
+  paste0(
+    make_line(2, o$line),
+    " ", o$left, " ",
+    make_line(o$width - ncl - ncr - 8, o$line),
+    " ", o$right, " ",
+    make_line(2, o$line)
+  )
+}
