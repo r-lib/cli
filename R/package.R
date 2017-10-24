165a0736e66192946dc2747ebc9862cd38636809
Gábor Csárdi <csardi.gabor@gmail.com>
2017-10-24 11:49:40 +0100

    Remove package doc, was outdated and redundant

--- R/package.R
+++
@@ -1,9 +0,0 @@
-
-#' Draw Boxes in the 'R' Console
-#'
-#' It integrates well with 'crayon' for coloring the boxes or their
-#' content, and 'clisymbols' to include Unicode characters in the boxes.
-#' It can also draw rules to make headers or footers in console output.
-#' @docType package
-#' @name boxes
-NULL
