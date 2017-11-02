40881f635c8224fc4096516749b011d7386a9792
Gábor Csárdi <csardi.gabor@gmail.com>
2017-11-02 13:44:09 +0000

    Run check tests in latin1 locale as well, if available

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,13 +2,32 @@ library(testthat)
 library(cli)
 
 ## Run the tests in fancy mode and non-fancy mode as well
-
-withr::with_options(
-  list(cli.unicode = TRUE),
-  test_check("cli")
-)
+## Also, run them in latin1 encoding as well, this is for Unix,
+## because Windows encoding names are different.
 
 withr::with_options(
   list(cli.unicode = FALSE),
-  test_check("cli")
+  test_check("cli", filter = "rules", reporter = "summary")
 )
+
+withr::with_options(
+  list(cli.unicode = TRUE),
+  test_check("cli", reporter = "summary")
+)
+
+has_locale <- function(l) {
+  has <- TRUE
+  tryCatch(
+    withr::with_locale(c(LC_CTYPE = l), "foobar"),
+    warning = function(w) has <<- FALSE,
+    error = function(e) has <<- FALSE
+  )
+  has
+}
+
+if (l10n_info()$`UTF-8` && has_locale("en_US.ISO8859-1")) {
+  withr::with_locale(
+    c(LC_CTYPE = "en_US.ISO8859-1"),
+    test_check("cli", reporter = "summary")
+  )
+}
