96d954adf9ef06a3485b0f1c88920108a7bada2e
Gábor Csárdi <csardi.gabor@gmail.com>
2017-11-03 12:32:16 +0000

    Add missing spinner test cases

--- tests/testthat/test-spinners.R
+++ tests/testthat/test-spinners.R
@@ -1,10 +1,17 @@
 
 context("spinners")
 
-test_that("list_spinners", {
+test_that("get_spinner", {
   if (fancy_boxes()) {
     expect_equal(get_spinner()$name, "dots")
   } else {
     expect_equal(get_spinner()$name, "line")
   }
+})
+
+test_that("list_spinners", {
+  ls <- list_spinners()
+  expect_true(is.character(ls))
+  expect_true("dots" %in% ls)
+  expect_true("line" %in% ls)
 })
