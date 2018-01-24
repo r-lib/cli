be8daf4e8fb438abb4ff1554c1d2f2b5c67da1bc
Gábor Csárdi <csardi.gabor@gmail.com>
2018-01-24 11:00:46 +0000

    Some cli components

--- R/themes.R
+++ R/themes.R
@@ -1,5 +1,36 @@
 
+#' @importFrom crayon combine_styles underline bold italic
+#'   green red yellow cyan
+
 cli_default_theme <- function() {
-  ## TODO
-  list()
+  list(
+    h1 = list(
+      fmt = combine_styles(bold, italic),
+      margin = list(top = 1, bottom = 1)),
+    h2 = list(
+      fmt = bold,
+      margin = list(top = 1, bottom = 1)),
+    h3 = list(
+      fmt = underline,
+      margin = list(top = 1, bottom = 0)),
+
+    text = list(),
+    verbatim = list(),
+
+    alert_success = list(
+      marker = symbol$tick,
+      fmt = green
+    ),
+    alert_danger = list(
+      marker = symbol$cross,
+      fmt = red
+    ),
+    alert_warning = list(
+      marker = symbol$warning,
+      fmt = yellow
+    ),
+    alert_info = list(
+      marker = symbol$info,
+      fmt = cyan)
+  )
 }
