d79b2a48b20a131b4b189bc9f41dd2f6721e29d9
Gábor Csárdi <csardi.gabor@gmail.com>
2019-09-12 11:18:23 +0100

    Add test case for non-breaking spaces
    Towards #86.

---
+++ tests/testthat/test-non-breaking-space.R
@@ -0,0 +1,13 @@
+
+context("non breaking space")
+
+test_that("does not break", {
+  op <- options(cli.width = 40)
+  on.exit(options(op))
+  str30 <- "123456789 123456789 1234567890"
+  out <- capt0(cli_text(c(str30, "this\u00a0is\u00a0not\u00a0breaking")))
+  expect_equal(
+    crayon::strip_style(out),
+    "123456789 123456789\n1234567890this\u00a0is\u00a0not\u00a0breaking\n"
+  )
+})
