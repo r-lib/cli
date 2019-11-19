7b6eefa1a736d81699b9730a71de1f6ce836291d
Gábor Csárdi <csardi.gabor@gmail.com>
2019-11-19 12:18:28 +0000

    Collapse inline substitutions
    Closes #88.

--- R/inline.R
+++ R/inline.R
@@ -8,37 +8,49 @@ inline_generic <- function(app, class, x) {
   on.exit(clii__container_end(app, id), add = TRUE)
   style <- app$get_current_style()
   xx <- paste0(style$before, x, style$after)
-  if (!is.null(style$fmt)) xx <- style$fmt(xx)
-  xx
+  if (!is.null(style$fmt)) xx <- vcapply(xx, style$fmt)
+  inline_collapse(xx)
+}
+
+inline_collapse <- function(x) {
+  glue_collapse(x, sep = ", ", last = " and ")
 }
 
 #' @importFrom glue glue glue_collapse
 
-inline_transformer <- function(code, envir) {
-  res <- tryCatch({
-    expr <- parse(text = code, keep.source = FALSE)
-    eval(expr, envir = envir)
-  }, error = function(e) e)
-  if (!inherits(res, "error")) return(res)
+inline_transformer <- local({
+  inline_styling <- FALSE
+  function(code, envir) {
+    res <- tryCatch({
+      expr <- parse(text = code, keep.source = FALSE)
+      eval(expr, envir = envir)
+    }, error = function(e) e)
+    if (!inherits(res, "error")) {
+      if (inline_styling) return(res) else return(inline_collapse(res))
+    }
 
-  code <- glue_collapse(code, "\n")
-  m <- regexpr("(?s)^[.]([[:alnum:]_]+)[[:space:]]+(.+)", code, perl = TRUE)
-  has_match <- m != -1
-  if (!has_match) stop(res)
+    code <- glue_collapse(code, "\n")
+    m <- regexpr("(?s)^[.]([[:alnum:]_]+)[[:space:]]+(.+)", code, perl = TRUE)
+    has_match <- m != -1
+    if (!has_match) stop(res)
 
-  starts <- attr(m, "capture.start")
-  ends <- starts + attr(m, "capture.length") - 1L
-  captures <- substring(code, starts, ends)
-  funname <- captures[[1]]
-  text <- captures[[2]]
+    starts <- attr(m, "capture.start")
+    ends <- starts + attr(m, "capture.length") - 1L
+    captures <- substring(code, starts, ends)
+    funname <- captures[[1]]
+    text <- captures[[2]]
 
-  out <- glue(text, .envir = envir, .transformer = inline_transformer)
-  inline_generic(app, funname, out)
-}
+    inline_styling <<- TRUE
+    on.exit(inline_styling <<- FALSE, add = TRUE)
+    out <- glue(text, .envir = envir, .transformer = inline_transformer)
+    inline_generic(app, funname, out)
+  }
+})
 
 clii__inline <- function(app, text, .list) {
-  ## This makes a copy that can refer to app
-  environment(inline_transformer) <- environment()
+  ## Inject that app, so we can style
+  assign("app", app, envir = environment(inline_transformer))
+  on.exit(rm(list = "app", envir = environment(inline_transformer)), add = TRUE)
   texts <- c(if (!is.null(text)) list(text), .list)
   out <- lapply(texts, function(t) {
     glue(t$str, .envir = t$values %||% emptyenv(), .transformer = inline_transformer)
---
+++ tests/testthat/test-collapsing.R
@@ -0,0 +1,19 @@
+
+setup(start_app())
+teardown(stop_app())
+
+test_that("collapsing without formatting", {
+  pkgs <- paste0("pkg", 1:5)
+  out <- capt0(cli_text("Packages: {pkgs}."), strip_style = TRUE)
+  expect_equal(out, "Packages: pkg1, pkg2, pkg3, pkg4 and pkg5.\n")
+})
+
+test_that("collapsing with formatting", {
+  cli_div(theme = list(".pkg" = list(fmt = function(x) paste0(x, " (P)"))))
+  pkgs <- paste0("pkg", 1:5)
+  out <- capt0(cli_text("Packages: {.pkg {pkgs}}."), strip_style = TRUE)
+  expect_equal(
+    out,
+    "Packages: pkg1 (P), pkg2 (P), pkg3 (P), pkg4 (P) and pkg5 (P).\n"
+  )
+})
