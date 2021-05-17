cd995def896207e4e2300e32dc35bc9a4ce43b55
Gábor Csárdi <csardi.gabor@gmail.com>
2021-05-17 16:07:25 +0200

    Update exported header file as well
    For the C API changes

--- inst/include/cli/progress.h
+++ inst/include/cli/progress.h
@@ -28,6 +28,62 @@ static R_INLINE SEXP cli_progress_bar(int total) {
   return ptr(&cli__should_tick, total);
 }
 
+static R_INLINE void cli_progress_set_name(SEXP bar, const char *name) {
+  static void (*ptr)(SEXP, const char*) = NULL;
+  if (ptr == NULL) {
+    ptr = (void (*)(SEXP, const char*))
+      R_GetCCallable("cli", "cli_progress_set_name");
+  }
+  ptr(bar, name);
+}
+
+static R_INLINE void cli_progress_set_status(SEXP bar, const char *name) {
+  static void (*ptr)(SEXP, const char*) = NULL;
+  if (ptr == NULL) {
+    ptr = (void (*)(SEXP, const char*))
+      R_GetCCallable("cli", "cli_progress_set_status");
+  }
+  ptr(bar, name);
+}
+
+static R_INLINE void cli_progress_set_type(SEXP bar, const char *name) {
+  static void (*ptr)(SEXP, const char*) = NULL;
+  if (ptr == NULL) {
+    ptr = (void (*)(SEXP, const char*))
+      R_GetCCallable("cli", "cli_progress_set_type");
+  }
+  ptr(bar, name);
+}
+
+static R_INLINE void cli_progress_set_format(SEXP bar, const char *name) {
+  static void (*ptr)(SEXP, const char*) = NULL;
+  if (ptr == NULL) {
+    ptr = (void (*)(SEXP, const char*))
+      R_GetCCallable("cli", "cli_progress_set_format");
+  }
+  ptr(bar, name);
+}
+
+static R_INLINE void cli_progress_set_estimate(SEXP bar,
+                                               int estimate,
+                                               int auto_estimate) {
+  static void (*ptr)(SEXP, int, int) = NULL;
+  if (ptr == NULL) {
+    ptr = (void (*)(SEXP, int, int))
+      R_GetCCallable("cli", "cli_progress_set_estimate");
+  }
+  ptr(bar, estimate, auto_estimate);
+}
+
+static R_INLINE void cli_progress_set_clear(SEXP bar, int clear) {
+  static void (*ptr)(SEXP, int) = NULL;
+  if (ptr == NULL) {
+    ptr = (void (*)(SEXP, int))
+      R_GetCCallable("cli", "cli_progress_set_clear");
+  }
+  ptr(bar, clear);
+}
+
 static R_INLINE void cli_progress_set(SEXP bar, int set) {
   static void (*ptr)(SEXP, int) = NULL;
   if (ptr == NULL) {
