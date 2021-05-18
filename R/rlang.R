2c58dbec4fac2ab6cc6a9ad796f4e391032b72bb
Gábor Csárdi <csardi.gabor@gmail.com>
2021-05-18 10:22:18 +0200

    Simplify rlang wrappers
    Now that we have format_error(), etc.

--- R/rlang.R
+++ R/rlang.R
@@ -30,93 +30,19 @@
 #' }
 
 cli_abort <- function(message, ..., .envir = parent.frame()) {
-  # Add "Error: " for the wrapping, because R adds it unconditionally
-  # TODO: I apparently can't translate this with
-  # gettext("Error: ", domain = "R")
-  message[1] <- paste0("Error: ", message[1])
-
-  # The default theme will make this bold
-  names(message)[1] <- "1"
-
-  rsconsole <- c("rstudio_console", "rstudio_console_starting")
-  if (rstudio_detect()$type %in% rsconsole) {
-    # leave some space for the traceback buttons in RStudio
-    oldopt <- options(cli.width = console_width() - 15L)
-    on.exit(options(oldopt), add = TRUE)
-  }
-
-  formatted1 <- fmt({
-    cli_div(class = "cli_rlang cli_abort")
-    cli_bullets(message, .envir = .envir)
-  }, collapse = TRUE)
-
-  # remove "Error: " that was only needed for the wrapping
-  formatted2 <- ansi_substr(formatted1, 8, nchar(formatted1))
-
-  formatted3 <- update_rstudio_color(formatted2)
-  rlang::abort(formatted3, ...)
+  rlang::abort(format_error(message, .envir = .envir), ...)
 }
 
 #' @rdname cli_abort
 #' @export
 
 cli_warn <- function(message, ..., .envir = parent.frame()) {
-  # The default theme will make this bold
-  names(message)[1] <- "1"
-
-  formatted1 <- fmt({
-    cli_div(class = "cli_rlang cli_warn")
-    cli_bullets(message, .envir = .envir)
-  }, collapse = TRUE)
-
-  formatted2 <- update_rstudio_color(formatted1)
-  rlang::warn(formatted2, ...)
+  rlang::warn(format_warning(message, .envir = .envir), ...)
 }
 
 #' @rdname cli_abort
 #' @export
 
 cli_inform <- function(message, ..., .envir = parent.frame()) {
-  formatted1 <- fmt({
-    cli_div(class = "cli_rlang cli_inform")
-    cli_bullets(message, .envir = .envir)
-  }, collapse = TRUE)
-  formatted2 <- update_rstudio_color(formatted1)
-  rlang::inform(formatted2, ...)
-}
-
-update_rstudio_color <- function(message) {
-  rscol <- get_rstudio_fg_color()
-  if (!is.null(rscol)) {
-    # in RStudio we only need to change the color
-    message[] <- rscol(message)
-  } else {
-    # in a terminal we need to undo the bold
-    message <- paste0(style_bold(""), message)
-  }
-  message
-}
-
-get_rstudio_fg_color <- function() {
-  tryCatch(
-    get_rstudio_fg_color0(),
-    error = function(e) NULL
-  )
-}
-
-get_rstudio_fg_color0 <- function() {
-  rs <- rstudio_detect()
-  oktypes <- c("rstudio_console", "rstudio_console_starting")
-  if (! rs$type %in% oktypes) return(NULL)
-  if (rs$num_colors == 1) return(NULL)
-  colstr <- rstudioapi::getThemeInfo()$foreground
-  if (is.null(colstr)) return(NULL)
-  colstr0 <- substr(colstr, 5, nchar(colstr) - 1)
-  rgbnum <- scan(text = colstr0, sep = ",", quiet = TRUE)
-  rgb <- grDevices::rgb(rgbnum[1]/255, rgbnum[2]/255, rgbnum[3]/255)
-  make_ansi_style(rgb)
-}
-
-rstudio_detect <- function() {
-  rstudio$detect()
+  rlang::inform(format_message(message, .envir = .envir), ...)
 }
--- tests/testthat/_snaps/rlang-errors.md
+++ tests/testthat/_snaps/rlang-errors.md
@@ -291,17 +291,15 @@
         cli_abort(c(lorem_ipsum(1, 3), i = lorem_ipsum(1, 3), x = lorem_ipsum(1, 3)))
       })
     Error <rlang_error>
-      Duis quis magna incididunt nulla commodo minim non
-      exercitation nostrud ullamco dolor exercitation ut veniam.
-      Fugiat irure tempor commodo voluptate ut. In et tempor excepteur
-      quis.
-      i Et nisi ad quis ad cupidatat tempor laborum est excepteur aliqua
-        veniam ex. Sunt magna veniam Lorem elit enim et pariatur
-        aliqua occaecat mollit consequat dolore in mollit. Officia
-        labore reprehenderit culpa dolore quis nisi do aliqua commodo
-        deserunt fugiat cupidatat nostrud ad.
-      x Ad laboris consectetur esse minim pariatur irure do anim anim.
-        Mollit ad cupidatat ullamco ullamco nulla elit in.
+      Duis quis magna incididunt nulla commodo minim non exercitation nostrud
+      ullamco dolor exercitation ut veniam. Fugiat irure tempor commodo voluptate ut.
+      In et tempor excepteur quis.
+      i Et nisi ad quis ad cupidatat tempor laborum est excepteur aliqua veniam ex.
+        Sunt magna veniam Lorem elit enim et pariatur aliqua occaecat mollit
+        consequat dolore in mollit. Officia labore reprehenderit culpa dolore quis
+        nisi do aliqua commodo deserunt fugiat cupidatat nostrud ad.
+      x Ad laboris consectetur esse minim pariatur irure do anim anim. Mollit ad
+        cupidatat ullamco ullamco nulla elit in.
 
 # color in RStudio [ansi]
 
