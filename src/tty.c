305c6784840bdafb2ddac8cb6c327d162e049c73
Gabor Csardi <csardi.gabor@gmail.com>
2021-06-30 10:08:35 +0100

    Fix terminal size detection on Solaris

--- src/tty.c
+++ src/tty.c
@@ -6,13 +6,14 @@
 #ifdef WIN32
 #include <windows.h>
 #else
+#include <termios.h>
 #include <sys/ioctl.h>
 #include <stdio.h>
 #include <unistd.h>
 #endif
 
 SEXP clic_tty_size() {
-  SEXP result = NULL;
+  SEXP result = PROTECT(Rf_allocVector(INTSXP, 2));
 
 #ifdef WIN32
   CONSOLE_SCREEN_BUFFER_INFO info;
@@ -21,18 +22,27 @@ SEXP clic_tty_size() {
     &info
   );
   if (!ok) R_THROW_SYSTEM_ERROR("Cannot determine terminal size");
-  result = Rf_allocVector(INTSXP, 2);
   INTEGER(result)[0] = info.srWindow.Right  - info.srWindow.Left + 1;
   INTEGER(result)[1] = info.srWindow.Bottom - info.srWindow.Top + 1;
-#else
+
+#elif defined(TIOCGWINSZ)
   struct winsize w;
   int err = ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-  if (err == -1) R_THROW_SYSTEM_ERROR("Cannot determine terminale size");
-
-  result = Rf_allocVector(INTSXP, 2);
+  if (err == -1) R_THROW_SYSTEM_ERROR("Cannot determine terminal size");
   INTEGER(result)[0] = w.ws_col;
   INTEGER(result)[1] = w.ws_row;
+
+#elif defined(TIOCGSIZE)
+  struct ttysize ts;
+  int err = ioctl(STDOUT_FILENO, TIOCGSIZE, &ts);
+  if (err == -1) R_THROW_SYSTEM_ERROR("Cannot determine terminal size");
+  INTEGER(result)[0] = ts.ts_cols;
+  INTEGER(result)[1] = ts.ts_rows;
+
+#else
+  R_THROW_ERROR("Don't know how to determine terminal size");
 #endif
 
+  UNPROTECT(1);
   return result;
 }
