d9733c7bce346fb67e42af4d6c179fca2857c849
Gábor Csárdi <csardi.gabor@gmail.com>
2022-01-20 10:57:17 +0100

    Qualify read.table()
    In case utils is not loaded.
    
    Probably not an R CMD check error because it runs at
    install time.
    
    Closes #393.

--- R/ansi-palette.R
+++ R/ansi-palette.R
@@ -89,12 +89,12 @@ truecolor <- as.integer(256 ^ 3)
 #' ansi_palette_show("dichro", colors = truecolor)
 
 ansi_palettes <- rbind(
-  read.table(
+  utils::read.table(
     "tools/ansi-palettes.txt",
     comment.char = ";",
     stringsAsFactors = FALSE
   ),
-  read.table(
+  utils::read.table(
     "tools/ansi-iterm-palettes.txt",
     comment.char = ";",
     stringsAsFactors = FALSE
