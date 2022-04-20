8c8c6baf6e33ef285103d121a0db33a98253e842
Gábor Csárdi <csardi.gabor@gmail.com>
2022-04-20 13:37:10 +0200

    Tests for ansi_ functions on hyperlinks

--- tests/testthat/test-ansi-hyperlink.R
+++ tests/testthat/test-ansi-hyperlink.R
@@ -1,18 +1,46 @@
 
 test_that("ansi_align", {
+  txt <- "\033]8;;https://ex.com\033\\text\033]8;;\033\\"
+  space3 <- strrep(" ", 3)
+  space6 <- strrep(" ", 6)
+  expect_equal(
+    ansi_align(txt, 10),
+    ansi_string(paste0(txt, space6))
+  )
 
+  expect_equal(
+    ansi_align(txt, 10, "center"),
+    ansi_string(paste0(space3, txt, space3))
+  )
+
+  expect_equal(
+    ansi_align(txt, 10, "right"),
+    ansi_string(paste0(space6, txt))
+  )
 })
 
 test_that("ansi_chartr", {
-
+  txt <- "1\033]8;;https://ex.com\033\\text\033]8;;\033\\2"
+  expect_equal(
+    ansi_chartr("12x0", "34yo", txt),
+    ansi_string("3\033]8;;https://ex.com\033\\teyt\033]8;;\033\\4")
+  )
 })
 
 test_that("ansi_columns", {
+  txt <- "\033]8;;https://ex.com\033\\text\033]8;;\033\\"
 
+  expect_equal(
+    ansi_strip(ansi_columns(rep(txt, 4), 10)),
+    c("text text ", "text text ")
+  )
 })
 
 test_that("ansi_has_any", {
-
+  txt <- "1\033]8;;https://ex.com\033\\text\033]8;;\033\\2"
+  expect_true(ansi_has_any(txt))
+  expect_true(ansi_has_any(txt, sgr = FALSE, csi = FALSE))
+  expect_false(ansi_has_any(txt, link = FALSE))
 })
 
 test_that("ansi_html", {
@@ -44,7 +72,7 @@ test_that("ansi_nchar", {
     list("\033]8;;https://ex.com\033\\text\033]8;;\033\\", 4),
     list("\033]8;x=1:y=2;https://ex.com\033\\text\033]8;;\033\\", 4),
     list("\033]8;;https://ex.com\033\\text\033]8;;\033\\", 4),
-    list("\033[1m\033]8;;https://ex.com\033\\text\033]8;;\033\\\033[22m", 4)    
+    list("\033[1m\033]8;;https://ex.com\033\\text\033]8;;\033\\\033[22m", 4)
   )
 
   for (c in cases) {
@@ -99,33 +127,91 @@ test_that("ansi_strip", {
 })
 
 test_that("ansi_strsplit", {
-
+  txt <- "1\033]8;;https://ex.com\033\\te;xt\033]8;;\033\\2"
+  expect_equal(
+    ansi_strsplit(txt, ";"),
+    list(ansi_string(c(
+      "1\033]8;;https://ex.com\033\\te\033]8;;\033\\",
+      "\033]8;;https://ex.com\033\\xt\033]8;;\033\\2"
+    )))
+  )
 })
 
 test_that("ansi_strtrim", {
-
+  txt <- "1\033]8;;https://ex.com\033\\text\033]8;;\033\\2"
+  expect_equal(
+    ansi_strtrim(txt, 6),
+    ansi_string(txt)
+  )
+  expect_equal(
+    ansi_strtrim(txt, 5),
+    ansi_string(paste0(ansi_substr(txt, 1, 2), "..."))
+  )
 })
 
 test_that("ansi_strwrap", {
-
+  txt <- "1\033]8;;https://ex.com\033\\text\033]8;;\033\\2"
+  wrp <- ansi_strwrap(strrep(paste0(txt, " "), 10), 15)
+  expect_equal(
+    wrp,
+    ansi_string(rep(paste0(txt, " ", txt), 5))
+  )
 })
 
 test_that("ansi_substr", {
+  txt <- "1\033[1m\033]8;;https://ex.com\033\\text\033]8;;\033\\\033[22m2"
 
+  cases <- list(
+    list(1, 3, "1\033]8;;https://ex.com\033\\\033[1mte\033[22m\033]8;;\033\\"),
+    list(4, 6, "\033]8;;https://ex.com\033\\\033[1mxt\033[22m\033]8;;\033\\2"),
+    list(3, 4, "\033]8;;https://ex.com\033\\\033[1mex\033[22m\033]8;;\033\\"),
+    list(1, 10, "1\033]8;;https://ex.com\033\\\033[1mtext\033[22m\033]8;;\033\\2")
+  )
+
+  for (c in cases) {
+    expect_equal(ansi_substr(txt, c[[1]], c[[2]]), ansi_string(c[[3]]))
+  }
 })
 
 test_that("ansi_substring", {
+  txt <- "1\033[1m\033]8;;https://ex.com\033\\text\033]8;;\033\\\033[22m2"
 
+  cases <- list(
+    list(1, 3, "1\033]8;;https://ex.com\033\\\033[1mte\033[22m\033]8;;\033\\"),
+    list(4, 6, "\033]8;;https://ex.com\033\\\033[1mxt\033[22m\033]8;;\033\\2"),
+    list(3, 4, "\033]8;;https://ex.com\033\\\033[1mex\033[22m\033]8;;\033\\"),
+    list(1, 10, "1\033]8;;https://ex.com\033\\\033[1mtext\033[22m\033]8;;\033\\2")
+  )
+
+  for (c in cases) {
+    expect_equal(ansi_substring(txt, c[[1]], c[[2]]), ansi_string(c[[3]]))
+  }
 })
 
 test_that("ansi_tolower", {
-
+  txt <- "Pre\033]8;;https://ex.com\033\\tEXt\033]8;;\033\\PoST"
+  expect_equal(
+    ansi_tolower(txt),
+    ansi_string("pre\033]8;;https://ex.com\033\\text\033]8;;\033\\post")
+  )
 })
 
 test_that("ansi_toupper", {
-
+  txt <- "Pre\033]8;;https://ex.com\033\\tEXt\033]8;;\033\\PoST"
+  expect_equal(
+    ansi_toupper(txt),
+    ansi_string("PRE\033]8;;https://ex.com\033\\TEXT\033]8;;\033\\POST")
+  )
 })
 
 test_that("ansi_trimws", {
-
+  txt <- "1\033[1m\033]8;;https://ex.com\033\\text\033]8;;\033\\\033[22m2"
+  expect_equal(
+    ansi_trimws(paste0("   ", txt, "   ")),
+    ansi_simplify(txt)
+  )
+  expect_equal(
+    ansi_trimws(txt),
+    ansi_string(txt)
+  )
 })
