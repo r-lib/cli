8d516287431f91254f67e3cc2ed3a7f2c559b0e3
Gábor Csárdi <csardi.gabor@gmail.com>
2022-08-18 09:28:20 +0200

    Add back all tests for #370

--- tests/testthat/test-glue.R
+++ tests/testthat/test-glue.R
@@ -9,17 +9,17 @@ test_that("glue quotes and comments", {
         "test_2" = "not #good"
       )
     )
-#    cli::cli_dl(c("test_3" = "no' good either"))
-#    cli::cli_dl(c("test_4" = "no\" good also"))
+    cli::cli_dl(c("test_3" = "no' good either"))
+    cli::cli_dl(c("test_4" = "no\" good also"))
     cli::cli_text("{.url https://example.com/#section}")
-#    cli::cli_alert_success("Qapla'")
+    cli::cli_alert_success("Qapla'")
   })
 })
 
 test_that("quotes, etc. within expressions are still OK", {
   expect_snapshot({
-#    cli::cli_text("{.url URL} {x <- 'foo'; nchar(x)}")
-#    cli::cli_text("{.url URL} {x <- \"foo\"; nchar(x)}")
+    cli::cli_text("{.url URL} {x <- 'foo'; nchar(x)}")
+    cli::cli_text("{.url URL} {x <- \"foo\"; nchar(x)}")
     cli::cli_text("{.url URL} {1 + 1 # + 1} {1 + 1}")
   })
 })
