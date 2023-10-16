17c2e2b4f744239357a2c2ded8911384d6cd9a43
Gábor Csárdi <csardi.gabor@gmail.com>
2023-10-16 10:48:22 +0200

    Theme component trees

--- R/components-map.R
+++ R/components-map.R
@@ -1,6 +1,6 @@
 map_component_tree <- function(cpt, parent = list()) {
   stopifnot(inherits(cpt, "cli_component"))
-  node <- new.env(parent = emptyenv())
+  node <- list()
   node[["component"]] <- cpt
   node[["tag"]] <- cpt[["tag"]]
   node[["id"]] <- cpt[["attr"]][["id"]]
@@ -12,6 +12,7 @@ map_component_tree <- function(cpt, parent = list()) {
   ))
 
   if (node[["tag"]] == "text") {
+    # TODO: need to handle embedded spans
     node[["children"]] <- cpt[["children"]]
   } else {
 
@@ -41,13 +42,14 @@ format.cli_component_tree <- function(x, ...) {
 }
 
 format_cli_component_tree <- function(x, root = FALSE, ...) {
+  themed <- if (isTRUE(x[["themed"]])) " (themed)" else ""
   if (x[["tag"]] == "text") {
-    c(if (root) "<cli_component_tree>",
+    c(if (root) paste0("<cli_component_tree>", themed),
       format(x[["component"]])
     )
   } else {
     id <- x[["id"]] %&&% paste0(" id=\"", x[["id"]], "\"")
-    c(if (root) "<cli_component_tree>",
+    c(if (root) paste0("<cli_component_tree>", themed),
       c(paste0("<", x[["tag"]], id, ">"),
         paste0("  ", unlist(lapply(x[["children"]], format_cli_component_tree))),
         paste0("</", x[["tag"]], ">")
@@ -61,4 +63,43 @@ format_cli_component_tree <- function(x, root = FALSE, ...) {
 print.cli_component_tree <- function(x, ...) {
   writeLines(format(x, ...))
   invisible(x)
+}
+
+theme_component_tree <- function(x, theme = list()) {
+  stopifnot(inherits(x, "cli_component_tree"))
+  parsed_sels <- lapply(as.character(names(theme)), parse_selector)
+  x[["style"]] <- x[["component"]][["attr"]][["style"]]
+  for (i in seq_along(parsed_sels)) {
+    if (match_selector(parsed_sels[[i]], x[["path"]])) {
+      x[["style"]] <- apply_theme_to_style(theme[[i]], x[["style"]])
+    }
+  }
+  x[["themed"]] <- TRUE
+
+  if (x[["tag"]] == "text") {
+    # TODO: handle embedded spans
+
+  } else {
+    x[["children"]] <- lapply(
+      x[["children"]],
+      theme_component_tree,
+      theme = theme
+    )
+  }
+
+  x
+}
+
+apply_theme_to_style <- function(style, theme) {
+  style <- as.list(style)
+  theme <- as.list(theme)
+
+  cm <- utils::modifyList(
+    theme$`class-map` %||% list(),
+    style$`class-map` %||% list()
+  )
+
+  merged <- modifyList(theme, style)
+  if (length(cm)) merged[["class-map"]] <- cm
+  merged
 }
