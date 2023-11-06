6ff9f136c4e1893392a630fff2b70900e28eea88
Gábor Csárdi <csardi.gabor@gmail.com>
2023-11-06 11:55:12 +0100

    cpt_* have class and style arguments now
    For simpler UI.

--- R/components.R
+++ R/components.R
@@ -6,13 +6,28 @@
 #' @param ... Subcomponents to add to the new component.
 #' @param children A list of subcomponents to add to the new component,
 #'   they are added after the ones in `...`.
+#' @param class Classes.
+#' @param style Styles.
 #' @param attr Attributes.
 #' @return A `div` component object.
 #'
 #' @export
 
-cpt_div <- function(..., children = NULL, attr = NULL) {
-  new_component("div", ..., children = children, attr = attr)
+cpt_div <- function(
+  ...,
+  children = NULL,
+  class = NULL,
+  style = NULL,
+  attr = NULL) {
+
+  new_component(
+    "div",
+    ...,
+    children = children,
+    class = class,
+    style = style,
+    attr = attr
+  )
 }
 
 #' @rdname cpt_div
@@ -29,13 +44,28 @@ div <- cpt_div
 #' @param ... Subcomponents to add to the new component.
 #' @param children A list of subcomponents to add to the new component,
 #'   they are added after the ones in `...`.
+#' @param class Classes.
+#' @param style Styles.
 #' @param attr Attributes.
 #' @return A `span` component object.
 #'
 #' @export
 
-cpt_span <- function(..., children = NULL, attr = NULL) {
-  new_component("span", ..., children = children, attr = attr)
+cpt_span <- function(
+  ...,
+  children = NULL,
+  class = NULL,
+  style = NULL,
+  attr = NULL
+) {
+  new_component(
+    "span",
+    ...,
+    children = children,
+    class = class,
+    style = style,
+    attr = attr
+  )
 }
 
 #' @rdname cpt_span
@@ -47,17 +77,25 @@ span <- cpt_span
 #' Create headers
 #'
 #' @param text Header text.
+#' @param class Classes.
+#' @param style Styles.
 #' @param attr Attributes.
 #' @param .envir Environment to evaluate interpolated expressions in.
 #' @return Header component.
 #'
 #' @export
 
-cpt_h1 <- function(text, attr = NULL, .envir = parent.frame()) {
+cpt_h1 <- function(
+  text,
+  class = NULL,
+  style = NULL,
+  attr = NULL,
+  .envir = parent.frame()
+) {
   if (is.character(text)) {
     text <- cpt_txt(text, .envir = .envir)
   }
-  new_component("h1", text, attr = attr)
+  new_component("h1", text, class = class, style = style, attr = attr)
 }
 
 #' @rdname cpt_h1
@@ -67,11 +105,17 @@ h1 <- cpt_h1
 #' @rdname cpt_h1
 #' @export
 
-cpt_h2 <- function(text, attr = NULL, .envir = parent.frame()) {
+cpt_h2 <- function(
+  text,
+  class = NULL,
+  style = NULL,
+  attr = NULL,
+  .envir = parent.frame()
+) {
   if (is.character(text)) {
     text <- cpt_txt(text, .envir = .envir)
   }
-  new_component("h2", text, attr = attr)
+  new_component("h2", text, class = class, style = style, attr = attr)
 }
 
 #' @rdname cpt_h1
@@ -102,6 +146,8 @@ h3 <- cpt_h3
 #' @param items,... List items, a list or a a character vector. Elements
 #'   must be strings or [list item components][cpt_li]. Character strings
 #'   can have interpreted `{}` literals.
+#' @param class Classes.
+#' @param style Styles.
 #' @param attr Attributes.
 #' @param .envir Environment to evaluate the interpreted `{}` literals in
 #'   items in.
@@ -109,7 +155,14 @@ h3 <- cpt_h3
 #'
 #' @export
 
-cpt_ul <- function(items, ..., attr = NULL, .envir = parent.frame()) {
+cpt_ul <- function(
+  items,
+  ...,
+  class = NULL,
+  style= NULL,
+  attr = NULL,
+  .envir = parent.frame()
+) {
   if (inherits(items, "cli_component_li")) {
     items <- list(items)
   }
@@ -127,7 +180,13 @@ cpt_ul <- function(items, ..., attr = NULL, .envir = parent.frame()) {
       item
     }
   })
-  new_component("ul", children = items, attr = attr)
+  new_component(
+    "ul",
+    children = items,
+    class = class,
+    style = style,
+    attr = attr
+  )
 }
 
 #' @rdname cpt_ul
@@ -142,6 +201,8 @@ ul <- cpt_ul
 #' @param items,... List items, a list or a a character vector. Elements
 #'   must be strings or [list item components][cpt_li]. Character strings
 #'   can have interpreted `{}` literals.
+#' @param class Classes.
+#' @param style Styles.
 #' @param attr Attributes.
 #' @param .envir Environment to evaluate the interpreted `{}` literals in
 #'   items in.
@@ -149,7 +210,14 @@ ul <- cpt_ul
 #'
 #' @export
 
-cpt_ol <- function(items, ..., attr = NULL, .envir = parent.frame()) {
+cpt_ol <- function(
+  items,
+  ...,
+  class = NULL,
+  style = NULL,
+  attr = NULL,
+  .envir = parent.frame()
+) {
   if (inherits(items, "cli_component_li")) {
     items <- list(items)
   }
@@ -167,7 +235,13 @@ cpt_ol <- function(items, ..., attr = NULL, .envir = parent.frame()) {
       item
     }
   })
-  new_component("ol", children = items, attr = attr)
+  new_component(
+    "ol",
+    children = items,
+    class = class,
+    style = style,
+    attr = attr
+  )
 }
 
 #' @rdname cpt_ol
@@ -181,13 +255,28 @@ ol <- cpt_ol
 #'
 #' @param ... Child components.
 #' @param children More child components, in a list.
+#' @param class Classes.
+#' @param style Styles.
 #' @param attr Attributes.
 #' @return A list item component, with class `cli_component_li`.
 #'
 #' @export
 
-cpt_li <- function(..., children = NULL, attr = NULL) {
-  new_component("li", ..., children = children, attr = attr)
+cpt_li <- function(
+  ...,
+  children = NULL,
+  class = NULL,
+  style = NULL,
+  attr = NULL
+) {
+  new_component(
+    "li",
+    ...,
+    children = children,
+    class = class,
+    style = style,
+    attr = attr
+  )
 }
 
 #' @rdname cpt_li
@@ -221,13 +310,26 @@ print.cli_component <- function(x, ...) {
 # Internals
 # -------------------------------------------------------------------------
 
-new_component <- function(tag, ..., children = NULL, attr = NULL) {
+new_component <- function(
+  tag,
+  ...,
+  children = NULL,
+  class = NULL,
+  style = NULL,
+  attr = NULL
+) {
   children <- c(list(...), children)
   if (tag == "text") {
     stopifnot(all(map_lgl(children, is_text_piece)))
   } else {
     stopifnot(all(map_lgl(children, inherits, "cli_component")))
   }
+
+  # TODO: merge classes?
+  attr[["class"]] <- class %||% attr[["class"]]
+  # This keeps NULL from attr[["style"]], unlike modifyList()
+  attr[["style"]][names(style)] <- style
+
   cpt <- new.env(parent = emptyenv())
   cpt[["tag"]] <- tag
   cpt[["children"]] <- children
